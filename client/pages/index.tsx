787dd8611c4024d50c581a250f766df1f2af5512
Nathan Thomas <nwthomas@me.com>
2021-09-25 23:08:28 -0700

    Adjust styling of layout

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,23 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
 import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
-    <Layout pageName="Home">
-      <Navbar />
-    </Layout>
+    <RootStyles>
+      <Layout pageName="Home">
+        <Navbar />
+      </Layout>
+    </RootStyles>
   );
 };
+
+const RootStyles = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 
 export default Home;
