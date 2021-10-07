0f08ef0cffb8ff126f5ba6e32931acaff36050b2
Nathan Thomas <nwthomas@me.com>
2021-10-06 18:48:53 -0700

    Update solidity contract

--- solidity-contracts/contracts/MessageHub.sol
+++ solidity-contracts/contracts/MessageHub.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MessageHub is Ownable {
-    constructor() {
-        // finish
+    struct Message {
+        string name;
+        string email;
+        string message;
+        string fax;
+    }
+
+    event NewMessage(Message);
+
+    function sendMessage(Message memory _newMessage) external returns (bool) {
+        if (keccak256(abi.encodePacked(_newMessage.fax)) == keccak256("")) {
+            return false;
+        }
+
+        emit NewMessage(_newMessage);
+        return true;
     }
 }
