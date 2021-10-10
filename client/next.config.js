969093a0c416edcf426f02976fecf742d0627f6d
Nathan Thomas <nwthomas@me.com>
2021-10-09 18:55:26 -0700

    Add Sentry to client

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
   reactStrictMode: true,
-}
+};
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true,
+};
+
+// This must run last before exporting
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
