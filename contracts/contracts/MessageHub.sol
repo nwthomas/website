dd9513e285c06e6c7f83f4ef381298cfb7810c51
Nathan Thomas <nwthomas@me.com>
2021-10-24 21:23:18 -0700

    Finalize contract MessageHub + add testing suite

--- contracts/contracts/MessageHub.sol
+++ contracts/contracts/MessageHub.sol
@@ -11,14 +11,10 @@ contract MessageHub is Ownable {
         string fax;
     }
 
-    event NewMessage(Message);
+    event NewMessage(string name, string email, string message);
 
-    function sendMessage(Message memory _newMessage) external returns (bool) {
-        if (keccak256(abi.encodePacked(_newMessage.fax)) != keccak256("")) {
-            return false;
-        }
-
-        emit NewMessage(_newMessage);
-        return true;
+    function sendMessage(Message memory _newMessage) external {
+        require(keccak256(abi.encodePacked(_newMessage.fax)) == keccak256(""), "Error: Message could not be sent");
+        emit NewMessage(_newMessage.name, _newMessage.email, _newMessage.message);
     }
 }
