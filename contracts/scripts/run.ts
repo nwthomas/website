a34cb5f5db7f4b52fd378cbc72e633294c5aabbe
Nathan Thomas <nwthomas@me.com>
2021-10-24 21:23:18 -0700

    Finalize contract MessageHub + add testing suite

--- contracts/scripts/run.ts
+++ contracts/scripts/run.ts
@@ -5,6 +5,30 @@ async function main() {
   const messageHub = await MessageHubFactory.deploy();
   await messageHub.deployed();
   console.log("MessageHub deployed to:", messageHub.address);
+
+  const mockValidMessage = {
+    name: "Test name field",
+    email: "Test email field",
+    message: "Test message field",
+    fax: "",
+  };
+
+  const mockInvalidMessage = {
+    name: "Test name field",
+    email: "Test email field",
+    message: "Test message field",
+    fax: "This field should not be filled",
+  };
+
+  let txn = await messageHub.sendMessage(mockValidMessage);
+  await txn.wait();
+
+  try {
+    txn = await messageHub.sendMessage(mockInvalidMessage);
+    await txn.wait();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main().catch((error) => {
--- contracts/test/MessageHub.test.ts
+++ contracts/test/MessageHub.test.ts
@@ -1,19 +1,61 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("MessageHub", function () {
+  let ownerAddress, secondAddress, thirdAddress;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  const getDeployedContract = async () => {
+    const MessageHubFactory = await ethers.getContractFactory("MessageHub");
+    const messageHub = await MessageHubFactory.deploy();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    return messageHub;
+  };
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  beforeEach(async () => {
+    const [owner, second, third] = await ethers.getSigners();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    ownerAddress = owner;
+    secondAddress = second;
+    thirdAddress = third;
+  });
+
+  it("should throw an error when an invalid message is sent", async () => {
+    const contract = await getDeployedContract();
+
+    const invalidMessage = {
+      name: "Test name field",
+      email: "Test email field",
+      message: "Test message field",
+      fax: "This field should not be filled",
+    };
+
+    let error;
+    try {
+      await contract.sendMessage(invalidMessage);
+    } catch (newError) {
+      error = newError;
+    }
+
+    expect(error instanceof Error).to.equal(true);
+    expect(
+      String(error).indexOf("Error: Message could not be sent") > -1
+    ).to.equal(true);
+  });
+
+  it("should emit an event when a valid message is sent", async () => {
+    const contract = await getDeployedContract();
+
+    const validMessage = {
+      name: "Test name field",
+      email: "Test email field",
+      message: "Test message field",
+      fax: "",
+    };
+
+    const txn = await contract.sendMessage(validMessage);
+    console.log(txn);
+    expect(txn)
+      .to.emit(contract, "NewMessage")
+      .withArgs(validMessage.name, validMessage.email, validMessage.message);
   });
 });
