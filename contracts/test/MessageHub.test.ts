d824692e89c455c11d448d6878dea1c815933aa1
Nathan Thomas <nwthomas@me.com>
2021-11-17 20:00:00 -0600

    Add more tests for MessageHub

--- contracts/test/MessageHub.test.ts
+++ contracts/test/MessageHub.test.ts
@@ -1,8 +1,11 @@
 import { expect } from "chai";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { ethers } from "hardhat";
 
 describe("MessageHub", function () {
-  let ownerAddress, secondAddress, thirdAddress;
+  let account1: SignerWithAddress;
+  let account2: SignerWithAddress;
+  let account3: SignerWithAddress;
 
   const getDeployedContract = async () => {
     const MessageHubFactory = await ethers.getContractFactory("MessageHub");
@@ -14,48 +17,108 @@ describe("MessageHub", function () {
   beforeEach(async () => {
     const [owner, second, third] = await ethers.getSigners();
 
-    ownerAddress = owner;
-    secondAddress = second;
-    thirdAddress = third;
+    account1 = owner;
+    account2 = second;
+    account3 = third;
   });
 
-  it("should throw an error when an invalid message is sent", async () => {
-    const contract = await getDeployedContract();
+  describe("ownership", () => {
+    it("instantiates a new contract with owner", async () => {
+      const project = await getDeployedContract();
+      const owner = await project.owner();
+      expect(owner).to.equal(account1.address);
+    });
 
-    const invalidMessage = {
-      name: "Test name field",
-      email: "Test email field",
-      message: "Test message field",
-      fax: "This field should not be filled",
-    };
+    it("transfers ownership", async () => {
+      const project = await getDeployedContract();
+      const transferOwnershipTxn = await project.transferOwnership(
+        account2.address
+      );
+      expect(transferOwnershipTxn)
+        .to.emit(project, "OwnershipTransferred")
+        .withArgs(account1.address, account2.address);
+    });
 
-    let error;
-    try {
-      await contract.sendMessage(invalidMessage);
-    } catch (newError) {
-      error = newError;
-    }
+    it("throws error when non-owner attempts transfer", async () => {
+      const project = await getDeployedContract();
 
-    expect(error instanceof Error).to.equal(true);
-    expect(
-      String(error).indexOf("Error: Message could not be sent") > -1
-    ).to.equal(true);
+      let error;
+      try {
+        await project.connect(account2).transferOwnership(account2.address);
+      } catch (newError) {
+        error = newError;
+      }
+
+      expect(
+        String(error).indexOf("Ownable: caller is not the owner") > -1
+      ).to.equal(true);
+    });
+
+    it("renounces ownership", async () => {
+      const project = await getDeployedContract();
+      const renounceOwnershipTxn = project.renounceOwnership();
+      expect(renounceOwnershipTxn)
+        .to.emit(project, "OwnershipTransferred")
+        .withArgs(
+          account1.address,
+          "0x0000000000000000000000000000000000000000"
+        );
+    });
+
+    it("throws error when non-owner attempts renouncing ownership", async () => {
+      const project = await getDeployedContract();
+
+      let error;
+      try {
+        await project.connect(account2).renounceOwnership();
+      } catch (newError) {
+        error = newError;
+      }
+
+      expect(
+        String(error).indexOf("Ownable: caller is not the owner") > -1
+      ).to.equal(true);
+    });
   });
 
-  it("should emit an event when a valid message is sent", async () => {
-    const contract = await getDeployedContract();
+  describe("messages", () => {
+    it("should throw an error when an invalid message is sent", async () => {
+      const contract = await getDeployedContract();
 
-    const validMessage = {
-      name: "Test name field",
-      email: "Test email field",
-      message: "Test message field",
-      fax: "",
-    };
+      const invalidMessage = {
+        name: "Test name field",
+        email: "Test email field",
+        message: "Test message field",
+        fax: "This field should not be filled",
+      };
 
-    const txn = await contract.sendMessage(validMessage);
-    console.log(txn);
-    expect(txn)
-      .to.emit(contract, "NewMessage")
-      .withArgs(validMessage.name, validMessage.email, validMessage.message);
+      let error;
+      try {
+        await contract.sendMessage(invalidMessage);
+      } catch (newError) {
+        error = newError;
+      }
+
+      expect(error instanceof Error).to.equal(true);
+      expect(
+        String(error).indexOf("Error: Message could not be sent") > -1
+      ).to.equal(true);
+    });
+
+    it("should emit an event when a valid message is sent", async () => {
+      const contract = await getDeployedContract();
+
+      const validMessage = {
+        name: "Test name field",
+        email: "Test email field",
+        message: "Test message field",
+        fax: "",
+      };
+
+      const txn = await contract.sendMessage(validMessage);
+      expect(txn)
+        .to.emit(contract, "NewMessage")
+        .withArgs(validMessage.name, validMessage.email, validMessage.message);
+    });
   });
 });
