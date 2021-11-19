23dfaadc5b33eb229b1b5e34168b729ec79aeaf3
Nathan Thomas <nwthomas@me.com>
2021-11-18 22:18:43 -0800

    Add code comments to contract

--- contracts/contracts/MessageHub.sol
+++ contracts/contracts/MessageHub.sol
@@ -13,6 +13,15 @@ contract MessageHub is Ownable {
 
     event NewMessage(string name, string email, string message);
 
+    /**
+     * @notice Allows the sending of a new message through the use of the EVM
+     * @param _newMessage The new message to be sent via an event. Fields include:
+     *   - name
+     *   - email
+     *   - message
+     *   - fax
+     * @dev The fax field is a honeypot field and thus reverts the entire function if present
+     */
     function sendMessage(Message memory _newMessage) external {
         require(keccak256(abi.encodePacked(_newMessage.fax)) == keccak256(""), "Error: Message could not be sent");
         emit NewMessage(_newMessage.name, _newMessage.email, _newMessage.message);
