ea3f6e089332be80c5a8dbd6065ae2cb507050b4
Nathan Thomas <nwthomas@me.com>
2021-11-30 20:00:00 -0600

    Add additional dev dependencies for contracts

--- contracts/contracts/MessageHub.sol
+++ contracts/contracts/MessageHub.sol
@@ -4,26 +4,29 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MessageHub is Ownable {
-    struct Message {
-        string name;
-        string email;
-        string message;
-        string fax;
-    }
+  struct Message {
+    string name;
+    string email;
+    string message;
+    string fax;
+  }
 
-    event NewMessage(string name, string email, string message);
+  event NewMessage(string name, string email, string message);
 
-    /**
-     * @notice Allows the sending of a new message through the use of the EVM
-     * @param _newMessage The new message to be sent via an event. Fields include:
-     *   - name
-     *   - email
-     *   - message
-     *   - fax
-     * @dev The fax field is a honeypot field and thus reverts the entire function if present
-     */
-    function sendMessage(Message memory _newMessage) external {
-        require(keccak256(abi.encodePacked(_newMessage.fax)) == keccak256(""), "Error: Message could not be sent");
-        emit NewMessage(_newMessage.name, _newMessage.email, _newMessage.message);
-    }
+  /**
+   * @notice Allows the sending of a new message through the use of the EVM
+   * @param _newMessage The new message to be sent via an event. Fields include:
+   *   - name
+   *   - email
+   *   - message
+   *   - fax
+   * @dev The fax field is a honeypot field and thus reverts the entire function if present
+   */
+  function sendMessage(Message memory _newMessage) external {
+    require(
+      keccak256(abi.encodePacked(_newMessage.fax)) == keccak256(""),
+      "Error: Message could not be sent"
+    );
+    emit NewMessage(_newMessage.name, _newMessage.email, _newMessage.message);
+  }
 }
