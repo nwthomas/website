21643d25ec56c8b56904411bff8bd65b83ace886
Nathan Thomas <nwthomas@me.com>
2021-12-20 11:34:48 -0800

    Update to use NODE_ENV NodeJS variable

--- api/middleware.ts
+++ api/middleware.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Express, Request, Response, Next } from "express";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 import { ALLOWLIST } from "./../constants/allowlist";
 dotenv.config();
 
-function isValidDomain(req, res, next) {
+function isValidDomain(req: Request, res: Response, next: Next) {
   if (ALLOWLIST.includes(req.get("origin"))) {
     next();
   } else {
@@ -17,7 +17,7 @@ function isValidDomain(req, res, next) {
   }
 }
 
-export default (server) => {
+export default (server: Express) => {
   server.use(express.json());
   server.use(cors());
   server.use(helmet());
