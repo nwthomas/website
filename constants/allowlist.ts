fce9968d25bdfc410e25081641d3dcace4c8ef77
Nathan Thomas <nwthomas@me.com>
2021-12-20 11:34:48 -0800

    Update to use NODE_ENV NodeJS variable

--- constants/allowlist.ts
+++ constants/allowlist.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ALLOWLIST = ["https://www.nathanthomas.dev"];
-if (process.env.DEV_ENV === "development") {
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "") {
   ALLOWLIST.push("http://localhost:3000");
 }
