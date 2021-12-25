58d16ebcf54874dcc495ceeddc05448f92f1c370
Nathan Thomas <nwthomas@me.com>
2021-12-25 13:45:43 -0800

    Attempt to fix background scroll with immovable BottomSheet

--- client/hooks/useLockBodyScroll.ts
+++ client/hooks/useLockBodyScroll.ts
@@ -5,23 +5,28 @@ export function useLockBodyScroll() {
     if (typeof window !== "undefined") {
       const getComputedStyle = (style: string): string =>
         window.getComputedStyle(document.body)[style];
+      const viewportTopYCoord = window.scrollY;
+      const viewportBottomYCoord = viewportTopYCoord + window.innerHeight;
 
       const originalBottomStyle = getComputedStyle("bottom");
       const originalLeftStyle = getComputedStyle("left");
       const originalOverflowStyle = getComputedStyle("overflow");
+      const originalPositionStyle = getComputedStyle("position");
       const originalRightStyle = getComputedStyle("right");
       const originalTopStyle = getComputedStyle("top");
 
-      document.body.style.bottom = "0";
+      document.body.style.bottom = String(viewportBottomYCoord);
       document.body.style.overflow = "hidden";
+      document.body.style.position = "fixed";
       document.body.style.left = "0";
       document.body.style.right = "0";
-      document.body.style.top = "0";
+      document.body.style.top = String(viewportTopYCoord);
 
       return () => {
         document.body.style.bottom = originalBottomStyle;
         document.body.style.left = originalLeftStyle;
         document.body.style.overflow = originalOverflowStyle;
+        document.body.style.position = originalPositionStyle;
         document.body.style.right = originalRightStyle;
         document.body.style.top = originalTopStyle;
       };
