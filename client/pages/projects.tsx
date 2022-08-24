9ae257276e04c83b7e7b5cf97e7c169f3ebf7832
Nathan Thomas <nwthomas@me.com>
2022-08-24 00:55:35 -0700

    Update project page emoji

--- client/pages/projects.tsx
+++ client/pages/projects.tsx
@@ -12,7 +12,7 @@ function Work() {
           <section>
             <h1>
               This is all the work that I'm really proud of. I hope you enjoy
-              it. 🌀
+              it. 🏗
             </h1>
           </section>
           <section>
