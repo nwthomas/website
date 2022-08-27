9ae18d3cbff522f0e26fb932ff8d9764ce91fffd
Nathan Thomas <nwthomas@me.com>
2022-08-27 00:02:46 -0700

    Update contract scripts and browser list

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,11 +1,12 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
 
 dotenv.config();
 
@@ -19,6 +20,10 @@ type ExtendedHardhatUserConfig = HardhatUserConfig & {
 const config: ExtendedHardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
+    mainnet: {
+      url: process.env.MAINNET_APP_SECRET_KEY || "",
+      chainId: 1,
+    },
     rinkeby: {
       url: process.env.RINKEBY_URL || "",
       accounts:
@@ -28,11 +33,13 @@ const config: ExtendedHardhatUserConfig = {
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled:
+      Boolean(process.env.REPORT_GAS) && process.env.REPORT_GAS !== "false",
     currency: "USD",
+    coinmarketcap: process.env.COIN_MARKET_CAP_API_KEY || "",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
   watcher: {
     compilation: {
@@ -48,7 +55,7 @@ const config: ExtendedHardhatUserConfig = {
           command: "test",
           params: {
             noCompile: true,
-            testFiles: ["./test/EthDistributor.test.js"],
+            testFiles: ["./test/MessageHub.test.js"],
           },
         },
       ],
