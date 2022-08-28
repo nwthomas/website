e27c2c0eb4620f85514078fef6d63edd66d46931
Nathan Thomas <nwthomas@me.com>
2022-08-28 14:28:29 -0700

    Add initial blog main page layout

--- client/pages/blog/index.tsx
+++ client/pages/blog/index.tsx
@@ -1,5 +1,51 @@
+import Layout from "../../components/Layout";
+import Link from "next/link";
+import styled from "styled-components";
+
+const PAGE_NAME = "Nathan Thomas | Blog";
+
 function Blog() {
-  return null;
+  return (
+    <Layout pageName={PAGE_NAME} withFooter>
+      <RootStyles>
+        <main>
+          <section>
+            <h1>I write a lot and publish most of it here. 📓</h1>
+          </section>
+        </main>
+      </RootStyles>
+    </Layout>
+  );
 }
+
+const RootStyles = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => `0 ${theme.appDimensions.appHorizontalGutters}`};
+  width: 100%;
+
+  > main {
+    max-width: ${({ theme }) => theme.appDimensions.appMaxWidth};
+
+    @media only screen and (min-width: ${({ theme }) =>
+        theme.breakpoints.tablet}) {
+      width: 100%;
+    }
+
+    > section {
+      display: flex;
+
+      @media only screen and (min-width: ${({ theme }) =>
+          theme.breakpoints.tablet}) {
+        margin-top: ${({ theme }) => theme.spaces.xxLarge};
+      }
+
+      > h1 {
+        background: ${({ theme }) => theme.colors.transparent};
+      }
+    }
+  }
+`;
 
 export default Blog;
