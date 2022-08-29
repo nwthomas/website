f386962874cb7eafec67d5bd13caf7b1202ad460
Nathan Thomas <nwthomas@me.com>
2022-08-28 18:42:32 -0700

    Update max font size <h1> and add new macbook psd file

--- client/styles/libs/global.ts
+++ client/styles/libs/global.ts
@@ -91,11 +91,6 @@ const GlobalStyle = css`
         theme.breakpoints.tablet}) {
       font-size: 5rem;
     }
-
-    @media only screen and (min-width: ${({ theme }) =>
-        theme.breakpoints.desktop}) {
-      font-size: 7rem;
-    }
   }
 
   h2 {
