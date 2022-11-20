ddf3f10c8d1a92d7a7998c6f6f93f00f5b4f8ac7
Nathan Thomas <nwthomas@me.com>
2022-11-19 19:34:04 -0800

    Add new theme transition switch button

--- client/components/ThemeTransitionSwitch/index.tsx
+++ client/components/ThemeTransitionSwitch/index.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 
-import { MoonIcon as Moon, SunIcon as Sun } from "../icons";
-import styled, { ThemeContext } from "styled-components";
+import { DARK_THEME, ThemeEnum } from "../../hooks/useGetPreferredTheme";
 
-import type { ThemeEnum } from "../../hooks/useGetPreferredTheme";
+import styled from "styled-components";
 
 interface Props {
   currentTheme: ThemeEnum | null;
@@ -14,49 +13,95 @@ export default function ThemeTransitionButton({
   onClick,
   currentTheme,
 }: Props) {
-  const theme = React.useContext(ThemeContext);
-  const MoonIcon = (
-    <Moon title="Dark mode icon" color={theme.colorsHex.royalBlue} />
-  );
-  const SunIcon = (
-    <Sun title="Light mode icon" color={theme.colorsHex.fireBush} />
-  );
-  const currentIcon = currentTheme === "dark" ? MoonIcon : SunIcon;
+  const isDarkMode = currentTheme === DARK_THEME;
 
   return (
-    <RootStyles>
+    <RootStyles isDarkMode={isDarkMode}>
       {currentTheme !== null ? (
-        <button onClick={onClick} role="button" tabIndex={0}>
-          {currentTheme ? currentIcon : null}
-        </button>
+        <>
+          <input
+            aria-label="Switch between light and dark mode"
+            type="checkbox"
+            checked={isDarkMode}
+            onChange={onClick}
+            tabIndex={0}
+          />
+          <div onClick={onClick}>
+            <p>🌙</p>
+            <p>☀️</p>
+            <div />
+          </div>
+        </>
       ) : null}
     </RootStyles>
   );
 }
 
-const RootStyles = styled.div`
-  align-items: center;
-  background: ${({ theme }) => theme.colors.transparent};
-  cursor: pointer;
-  display: flex;
-  justify-content: flex-end;
-  text-decoration: none;
-  transition: opacity ${({ theme }) => theme.transitions.medium} ease-in-out,
-    transform ${({ theme }) => theme.transitions.medium} ease-in-out;
-  width: 100%;
+type StyleProps = {
+  isDarkMode: boolean;
+};
 
-  &:hover {
-    opacity: ${({ theme }) => theme.opacity.opacity90};
+const RootStyles = styled.div<StyleProps>`
+  height: 24px;
+  width: 50px;
+  margin-bottom: 4px;
+
+  > input {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+
+    &:active + div > div,
+    &:focus + div > div {
+      box-shadow: 0 0 2px 3px ${({ theme }) => theme.colorsHex.pictonBlue};
+    }
   }
 
-  > button {
-    background: none;
-    border: none;
-    color: inherit;
+  > div {
+    background-color: ${({ theme }) => theme.colorsHex.black};
+    border: 1px solid ${({ theme }) => theme.colors.bodyBackgroundAccentTwo};
+    border-radius: ${({ theme }) => theme.borderRadii.infinity};
     cursor: pointer;
-    font: inherit;
-    height: 30px;
-    padding: 0;
-    width: 30px;
+    display: flex;
+    align-items: center;
+    height: 24px;
+    justify-content: space-between;
+    padding: 4px 3px 0;
+    position: relative;
+    width: 50px;
+
+    p {
+      font-size: 1.6rem;
+      text-align: center;
+      user-select: none;
+      vertical-align: middle;
+    }
+
+    > p:nth-child(2) {
+      font-size: 1.9rem;
+    }
+
+    > div {
+      background-color: ${({ theme }) => theme.colorsHex.white};
+      border-radius: ${({ theme }) => theme.borderRadii.infinity};
+      height: 20px;
+      position: absolute;
+      left: 2px;
+      top: 1px;
+      transform: translateX(${({ isDarkMode }) => (isDarkMode ? "24px" : 0)});
+      transition: transform ${({ theme }) => theme.transitions.short}
+        cubic-bezier(0.23, 1, 0.32, 1);
+      width: 20px;
+
+      &:active,
+      &:focus {
+        box-shadow: 0 0 2px 3px ${({ theme }) => theme.colorsHex.pictonBlue};
+      }
+    }
   }
 `;
