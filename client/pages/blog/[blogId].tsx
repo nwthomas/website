c0df547b58c466a0a72807930465eece6661bacb
Nathan Thomas <nwthomas@me.com>
2022-12-10 01:29:44 -0800

    Add initial CSS for blog page

--- client/pages/blog/[blogId].tsx
+++ client/pages/blog/[blogId].tsx
@@ -2,6 +2,7 @@ import { BlogPosts, buildSlugToBlogPostMap } from "../../utils/sortBlogPosts";
 
 import Layout from "../../components/Layout";
 import { getDirectoryFiles } from "../../utils/readBlogFiles";
+import styled from "styled-components";
 import { useRouter } from "next/router";
 
 export async function getStaticProps() {
@@ -44,9 +45,39 @@ function BlogPost({ slugToBlogPostMap }) {
 
   return (
     <Layout pageName={""} withFooter>
-      <p>Testing</p>
+      <RootStyles>
+        <main>
+          <p>Testing</p>
+        </main>
+      </RootStyles>
     </Layout>
   );
 }
+
+const RootStyles = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => `0 ${theme.appDimensions.appHorizontalGutters}`};
+  width: 100%;
+
+  > main {
+    margin-bottom: ${({ theme }) => theme.spaces.large};
+    max-width: ${({ theme }) => theme.appDimensions.appMaxWidth};
+
+    @media only screen and (min-width: ${({ theme }) =>
+        theme.breakpoints.tablet}) {
+      margin-bottom: ${({ theme }) =>
+        `calc(${theme.spaces.medium} + ${theme.spaces.xxLarge})`};
+      margin-top: ${({ theme }) => theme.spaces.medium};
+    }
+
+    @media only screen and (min-width: ${({ theme }) =>
+        theme.breakpoints.tablet}) {
+      padding-top: ${({ theme }) => theme.spaces.medium};
+      width: 100%;
+    }
+  }
+`;
 
 export default BlogPost;
