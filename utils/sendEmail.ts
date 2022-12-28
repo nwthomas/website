0b27a9833851a0c468c5b1a05617ea65b689e674
Nathan Thomas <nwthomas@me.com>
2022-12-27 23:12:15 -0800

    Update README

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,4 +1,3 @@
-import cryptojs from "crypto-js";
 import nodemailer from "nodemailer";
 
 const SECRET_KEY = process.env.SECRET_KEY || "";
@@ -9,24 +8,8 @@ interface NewEmail {
   message: string;
 }
 
-export const sendEmail = async (newEmail: NewEmail, withWeb3?: boolean) => {
-  const { name: rawName, email: rawEmail, message: rawMessage } = newEmail;
-
-  let name = rawName;
-  let email = rawEmail;
-  let message = rawMessage;
-  // Client-side encodes the values for a bit of extra protection with events being
-  // logged on the Ethereum blockchain, so this merely decrypts them if necessary
-  if (withWeb3) {
-    const nameBytes = cryptojs.AES.decrypt(name, SECRET_KEY);
-    name = JSON.parse(nameBytes.toString(cryptojs.enc.Utf8));
-
-    const emailBytes = cryptojs.AES.decrypt(email, SECRET_KEY);
-    email = JSON.parse(emailBytes.toString(cryptojs.enc.Utf8));
-
-    const messageBytes = cryptojs.AES.decrypt(message, SECRET_KEY);
-    message = JSON.parse(messageBytes.toString(cryptojs.enc.Utf8));
-  }
+export const sendEmail = async (newEmail: NewEmail) => {
+  const { name, email, message } = newEmail;
 
   const transporter = nodemailer.createTransport({
     host: process.env.HOST_NAME,
@@ -38,8 +21,7 @@ export const sendEmail = async (newEmail: NewEmail, withWeb3?: boolean) => {
     },
   });
 
-  const subjectEnding = withWeb3 ? " (through smart contract)" : "";
-  const subject = `From ${name}${subjectEnding}`;
+  const subject = `From ${name}`;
 
   const result = await transporter.sendMail({
     from: process.env.PERSONAL_EMAIL,
