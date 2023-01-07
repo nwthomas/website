31c25894a93f9e15f5bae18c5fbc094679ce34e6
Nathan Thomas <nwthomas@me.com>
2023-01-07 11:27:31 -0500

    Add util for building meta tag image urls

--- client/components/SEO/index.tsx
+++ client/components/SEO/index.tsx
@@ -4,10 +4,17 @@ import Head from "next/head";
 import { buildSeoConfig } from "../../constants/seo";
 import { useGetPageName } from "../../hooks";
 
+function buildImageUrlWithOrigin(imageUrl: string): string {
+  const origin = typeof window !== 'undefined' && window.location.origin
+    ? window.location.origin
+    : '';
+
+  return `${origin}${imageUrl}`
+}
+
 interface Props {
   customDescription?: string;
   customImageUrl?: string;
-  isArticle?: boolean;
   pageName: string;
   withPageNameEmojis?: boolean;
 }
@@ -15,14 +22,9 @@ interface Props {
 function SEO({
   customDescription,
   customImageUrl,
-  isArticle,
   pageName,
   withPageNameEmojis,
 }: Props) {
-  const origin = typeof window !== 'undefined' && window.location.origin
-    ? window.location.origin
-    : '';
-
   const currentPageMetadata = React.useMemo(() => {
     return buildSeoConfig(pageName);
   }, [pageName]);
@@ -40,28 +42,29 @@ function SEO({
 
   return (
     <Head>
-      <title>{tabTitle}</title>
 
       {/* Miscellaneous Meta Tags */}
+      <title>{tabTitle}</title>
       <meta charSet="utf-8" />
       <meta name="description" content={customDescription || description} />
-      <meta name="image" content={customImageUrl || imageUrl} />
+      <meta name="image" content={buildImageUrlWithOrigin(customImageUrl || imageUrl)} />
 
       {/* Open Graph Meta Tags */}
-      <meta property="og:type" content={isArticle ? "article" : "website"} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={currentUrl} />
       <meta property="og:title" content={pageName} />
       <meta property="og:description" content={customDescription || description} />
-      <meta property="og:image" content={`${origin}${customImageUrl || imageUrl}`} />
-      <meta property="og:url" content={currentUrl} />
+      <meta property="og:image" content={buildImageUrlWithOrigin(customImageUrl || imageUrl)} />
       <meta property="og:site_name" content={siteName} />
 
       {/* Twitter Meta Tags */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content={twitterHandle} />
-      <meta name="twitter:creator" content={twitterHandle} />
+      <meta name="twitter:url" content={currentUrl} />
       <meta name="twitter:title" content={pageName} />
       <meta name="twitter:description" content={customDescription || description} />
-      <meta name="twitter:image" content={`${origin}${customImageUrl || imageUrl}`} />
+      <meta name="twitter:site" content={twitterHandle} />
+      <meta name="twitter:creator" content={twitterHandle} />
+      <meta name="twitter:image" content={buildImageUrlWithOrigin(customImageUrl || imageUrl)} />
     </Head>
   );
 }
