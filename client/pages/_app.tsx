2ebd6b17df07f6fa9d5ad305434755de1a8e0477
Nathan Thomas <nwthomas@me.com>
2023-01-28 23:33:10 -0800

    Fix logging bug

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -31,8 +31,6 @@ const client = new QueryClient({
 function App({ Component, pageProps }: AppProps) {
   const [currentThemeFromRedux] = useTheme();
 
-  console.log(currentThemeFromRedux);
-
   const mainTheme = React.useMemo(() => {
     if (!currentThemeFromRedux) {
       const currentThemeFromWindow = getThemeFromWindowObject();
