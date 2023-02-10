4d2a76bd41000c71df8768381efa497d31807296
Nathan Thomas <nwthomas@me.com>
2023-02-10 01:55:37 -0800

    Fix safe area colors

--- client/components/SEO/index.tsx
+++ client/components/SEO/index.tsx
@@ -86,7 +86,9 @@ function SEO({
           <meta
             name="theme-color"
             content={
-              currentTheme === DARK_THEME ? colorsHex.black : colorsHex.white
+              currentTheme === DARK_THEME
+                ? colorsHex.codGray
+                : colorsHex.alabaster
             }
           ></meta>
         ) : null}
