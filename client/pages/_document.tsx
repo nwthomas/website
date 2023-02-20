33cbcedc4dcdf40fe21ccba01ad3c2e909606829
Nathan Thomas <nwthomas@me.com>
2023-02-19 19:05:49 -0800

    Add viewport meta tag to avoid defaults

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -33,6 +33,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
           <link
             rel="preload"
             href="/fonts/LibreBaskervilleBold.woff2"
