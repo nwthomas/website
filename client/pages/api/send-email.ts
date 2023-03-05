3e628d737710beafcdd022625cb7f3f2ed0d1e36
Nathan Thomas <nwthomas@me.com>
2023-03-05 12:32:10 -0800

    Set up rate limiting on contact form

--- client/pages/api/send-email.ts
+++ client/pages/api/send-email.ts
@@ -1,25 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { applyRateLimit } from "./../../utils/rateLimit";
 import { sendEmail } from "../../utils/sendEmail";
 
 const POST_METHOD = "POST";
+const STATUS_200 = "The message was sent successfully.";
+const STATUS_404 = "The message could not be sent.";
+const STATUS_405 = "This request type is not supported.";
+const STATUS_429 = "Too many requests";
+const STATUS_502 = "An error occurred while sending the message.";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+  request: NextApiRequest,
+  response: NextApiResponse
 ) {
-  if (req.method !== POST_METHOD) {
-    return res.status(405).send({
-      message: "This request type is not supported.",
+  if (request.method !== POST_METHOD) {
+    return response.status(405).send({
+      message: STATUS_405,
       success: false,
     });
   }
 
-  const { email, fax, message, name } = req.body;
+  try {
+    await applyRateLimit(request, response);
+  } catch (error) {
+    return response.status(429).send(STATUS_429);
+  }
+
+  const { email, fax, message, name } = request.body;
 
   if (fax?.length) {
-    return res.status(404).send({
-      message: "The message could not be sent.",
+    return response.status(404).send({
+      message: STATUS_404,
       success: false,
     });
   }
@@ -27,13 +39,13 @@ export default async function handler(
   const result = await sendEmail({ name, email, message });
 
   if (result) {
-    return res.status(200).send({
-      message: "The message was sent successfully.",
+    return response.status(200).send({
+      message: STATUS_200,
       success: true,
     });
   } else {
-    return res.status(502).send({
-      message: "An error occurred while sending the message.",
+    return response.status(502).send({
+      message: STATUS_502,
       success: false,
     });
   }
