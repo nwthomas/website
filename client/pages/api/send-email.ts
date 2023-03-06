3dc8dd728a01497d7204c101746aeb877be337f6
Nathan Thomas <nwthomas@me.com>
2023-03-05 16:30:08 -0800

    Adjust rate limiting to work properly

--- client/pages/api/send-email.ts
+++ client/pages/api/send-email.ts
@@ -1,4 +1,7 @@
-// This is not ideal as it would be better to use the NextJS-defined types
+// This is not ideal as it would be better to use the NextJS-defined types, but doing this allows
+// the rate limiting middleware to not need type coercion. The difference between the ExpressJS
+// and NextJS types are minimul (as NextJS has some additional values). TypeScript seems to be
+// happy here, so I'm leaving it as-is for now.
 import { Request, Response } from "express";
 
 import { applyRateLimit } from "./../../utils/rateLimit";
@@ -9,9 +12,13 @@ const STATUS_200 = "The message was sent successfully.";
 const STATUS_404 = "The message could not be sent.";
 const STATUS_405 = "This request type is not supported.";
 const STATUS_429 = "Too many requests";
+const STATUS_500 = "Internal server error";
 const STATUS_502 = "An error occurred while sending the message.";
 
 export default async function handler(request: Request, response: Response) {
+  // Return early if not POST request which is the only type the client has been set
+  // up to send. This means it's a user sending a request via a man-in-the-middle
+  // (proxy request).
   if (request.method !== POST_METHOD) {
     return response.status(405).send({
       message: STATUS_405,
@@ -22,6 +29,7 @@ export default async function handler(request: Request, response: Response) {
   try {
     await applyRateLimit(request, response);
   } catch (error) {
+    console.log({ error });
     return response.status(429).send({
       message: STATUS_429,
       success: false,
@@ -30,6 +38,7 @@ export default async function handler(request: Request, response: Response) {
 
   const { email, fax, message, name } = request.body;
 
+  // If the fax string exists and length is greater than 0, this is a bot request
   if (fax?.length) {
     return response.status(404).send({
       message: STATUS_404,
@@ -37,16 +46,23 @@ export default async function handler(request: Request, response: Response) {
     });
   }
 
-  const result = await sendEmail({ name, email, message });
+  try {
+    const result = await sendEmail({ name, email, message });
 
-  if (result) {
-    return response.status(200).send({
-      message: STATUS_200,
-      success: true,
-    });
-  } else {
-    return response.status(502).send({
-      message: STATUS_502,
+    if (result) {
+      return response.status(200).send({
+        message: STATUS_200,
+        success: true,
+      });
+    } else {
+      return response.status(502).send({
+        message: STATUS_502,
+        success: false,
+      });
+    }
+  } catch (error) {
+    return response.status(500).send({
+      message: STATUS_500,
       success: false,
     });
   }
