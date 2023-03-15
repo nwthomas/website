675f999aadd63bbce9cf15b24a41b0c031dd3120
Nathan Thomas <nwthomas@me.com>
2023-03-14 23:15:26 -0700

    Finish desktop layout for new home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -77,19 +77,19 @@ const Home: NextPage = () => {
                   width="1365"
                 />
               </div>
-              <div>
-                <Image
-                  alt="Loom logo"
-                  height="333"
-                  loading="eager"
-                  priority
-                  quality={100}
-                  src="/images/backgrounds/loom-logo.webp"
-                  width="333"
-                />
-              </div>
               <div />
               <div>
+                <div>
+                  <Image
+                    alt="Loom logo"
+                    height="333"
+                    loading="eager"
+                    priority
+                    quality={100}
+                    src="/images/backgrounds/loom-logo.webp"
+                    width="333"
+                  />
+                </div>
                 <ComputerScreen
                   imageAlt="Computer screen displaying Loom web app video trimming"
                   imageHeight={481}
@@ -186,7 +186,7 @@ const RootStyles = styled.div`
 
     > section:nth-child(2) {
       display: flex;
-      height: 800px;
+      height: 750px;
       margin-bottom: ${({ theme }) => theme.spaces.medium};
       position: relative;
 
@@ -210,17 +210,8 @@ const RootStyles = styled.div`
         z-index: 1;
       }
 
-      /* Rotating Loom logo */
-      > div:nth-child(2) {
-        animation: ${rotateAnimation} 60s linear infinite;
-        position: absolute;
-        right: ${({ theme }) => theme.spaces.nano};
-        top: ${({ theme }) => `calc(${theme.spaces.xLarge} * 3)`};
-        z-index: 1;
-      }
-
       /* Solid color background */
-      > div:nth-child(3) {
+      > div:nth-child(2) {
         background-color: ${({ theme }) =>
           theme.colors.bodyBackgroundAccentThree};
         border-radius: ${({ theme }) => theme.borderRadii.large};
@@ -235,7 +226,7 @@ const RootStyles = styled.div`
       }
 
       /* Mock computer window */
-      > div:nth-child(4) {
+      > div:nth-child(3) {
         bottom: ${({ theme }) =>
           `calc(${theme.spaces.xxLarge} + ${theme.spaces.medium})`};
         position: absolute;
@@ -243,13 +234,22 @@ const RootStyles = styled.div`
           `calc(${theme.spaces.large} + ${theme.spaces.medium})`};
         width: calc(55% - ${({ theme }) => theme.spaces.medium} * 2);
         z-index: 2;
+
+        /* Rotating Loom logo */
+        > div:nth-child(1) {
+          animation: ${rotateAnimation} 60s linear infinite;
+          position: absolute;
+          right: ${({ theme }) => `-${theme.spaces.xLarge}`};
+          top: ${({ theme }) => `calc(-${theme.spaces.large} * 2)`};
+          z-index: -1;
+        }
       }
 
       /* Iphone demo */
-      > div:nth-child(5) {
+      > div:nth-child(4) {
         position: absolute;
         left: 10%;
-        top: ${({ theme }) => theme.spaces.xLarge};
+        top: ${({ theme }) => theme.spaces.medium};
         width: 25%;
         z-index: 3;
 
