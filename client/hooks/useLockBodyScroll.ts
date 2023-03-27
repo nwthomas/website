6c5bc4829b36fb2ece8411db955134acdfff500e
Nathan Thomas <nwthomas@me.com>
2023-03-26 18:31:44 -0700

    Add basics for mobile nav and nav menu

--- client/hooks/useLockBodyScroll.ts
+++ client/hooks/useLockBodyScroll.ts
@@ -1,33 +1,44 @@
 import * as React from "react";
 
+// export function useLockBodyScroll() {
+//   React.useEffect(() => {
+//     if (typeof window !== "undefined") {
+//       const getComputedStyle = (style: string): string =>
+//         window.getComputedStyle(document.body)[style];
+
+//       const originalBottomStyle = getComputedStyle("bottom");
+//       const originalLeftStyle = getComputedStyle("left");
+//       const originalOverflowStyle = getComputedStyle("overflow");
+//       const originalPositionStyle = getComputedStyle("position");
+//       const originalRightStyle = getComputedStyle("right");
+//       const originalTopStyle = getComputedStyle("top");
+
+//       document.body.style.bottom = "0";
+//       document.body.style.overflow = "hidden";
+//       document.body.style.position = "fixed";
+//       document.body.style.left = "0";
+//       document.body.style.right = "0";
+//       document.body.style.top = "0";
+
+//       return () => {
+//         document.body.style.bottom = originalBottomStyle;
+//         document.body.style.left = originalLeftStyle;
+//         document.body.style.overflow = originalOverflowStyle;
+//         document.body.style.position = originalPositionStyle;
+//         document.body.style.right = originalRightStyle;
+//         document.body.style.top = originalTopStyle;
+//       };
+//     }
+//   }, []);
+// }
+
 export function useLockBodyScroll() {
-  React.useEffect(() => {
-    if (typeof window !== "undefined") {
-      const getComputedStyle = (style: string): string =>
-        window.getComputedStyle(document.body)[style];
+  React.useLayoutEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
 
-      const originalBottomStyle = getComputedStyle("bottom");
-      const originalLeftStyle = getComputedStyle("left");
-      const originalOverflowStyle = getComputedStyle("overflow");
-      const originalPositionStyle = getComputedStyle("position");
-      const originalRightStyle = getComputedStyle("right");
-      const originalTopStyle = getComputedStyle("top");
-
-      document.body.style.bottom = "0";
-      document.body.style.overflow = "hidden";
-      document.body.style.position = "fixed";
-      document.body.style.left = "0";
-      document.body.style.right = "0";
-      document.body.style.top = "0";
-
-      return () => {
-        document.body.style.bottom = originalBottomStyle;
-        document.body.style.left = originalLeftStyle;
-        document.body.style.overflow = originalOverflowStyle;
-        document.body.style.position = originalPositionStyle;
-        document.body.style.right = originalRightStyle;
-        document.body.style.top = originalTopStyle;
-      };
-    }
+    return () => {
+      document.body.style.overflow = originalStyle;
+    };
   }, []);
 }
