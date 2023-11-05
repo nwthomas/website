a4b0987040a3d7b262560a37cac48bcc716eecf9
Nathan Thomas <nwthomas@me.com>
2023-11-04 23:56:59 -0700

    Add squid to 404 page

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -15,7 +15,7 @@ function FourOhFour() {
   return (
     <Layout pageName={FOUR_OH_FOUR_PAGE_NAME} withFooter>
       <main id={CONTENTS_ID}>
-        <Error errorCode="404" />
+        <Error errorCode="🦑 404" />
       </main>
     </Layout>
   );
