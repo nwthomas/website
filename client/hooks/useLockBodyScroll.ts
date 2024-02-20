fc15286838e00030fed0672d9ce1d90fe8eff6f7
Nathan Thomas <nwthomas@me.com>
2024-02-19 23:53:55 -0800

    Add magnifying glass for image hover

--- client/hooks/useLockBodyScroll.ts
+++ client/hooks/useLockBodyScroll.ts
@@ -1,12 +1,16 @@
-import * as React from "react";
+import { useLayoutEffect } from "react";
 
 export function useLockBodyScroll() {
-  React.useLayoutEffect(() => {
+  useLayoutEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
+    const scrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
     document.body.style.overflow = "hidden";
 
     return () => {
       document.body.style.overflow = originalStyle;
+      window.scrollTo(0, scrollPosition);
     };
   }, []);
 }
