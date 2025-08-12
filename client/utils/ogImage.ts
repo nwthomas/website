ff4d603656ba4951e14231b017d6c6a9f6db11b5
Nathan Thomas <nwthomas@me.com>
2025-08-12 13:21:24 -0700

    Revert to Node 20 for now

--- client/utils/ogImage.ts
+++ client/utils/ogImage.ts
@@ -2,12 +2,13 @@ import { OG_IMAGE_HEIGHT, OG_IMAGE_WIDTH } from "../pages/og-image";
 
 import { DEFAULT_SEO_VALUES } from "../constants/seo";
 import { ORIGIN } from "../constants/routes";
+// This solution was originally based off of:
+// https://github.com/JupiterOne/playwright-aws-lambda/issues/15#issuecomment-1243395780
+import chromium from "@sparticuz/chromium";
 import { createHash } from "crypto";
 import fs from "fs";
 import { isProductionEnvironment } from "./../constants/environments";
-// This solution was pulled from:
-// https://github.com/JupiterOne/playwright-aws-lambda/issues/15#issuecomment-1243395780
-import { launchChromium } from "playwright-aws-lambda/dist/src/chromium";
+import { chromium as playwright } from "playwright-core";
 
 // While Vercel has an edge function way of rendering dynamic OG images, I didn't want to deal with edge function.
 // Instead, I generate OG images at build time and thereby offload any future costs of image generation to Vercel.
@@ -22,23 +23,27 @@ export async function createOgImage(path: string) {
 
   const url = `${ORIGIN}${path}`;
   const hash = createHash("md5").update(url).digest("hex");
-  const browser = await launchChromium({ headless: true });
   const ogImageDir = "./public/images/og";
-
-  console.log(`Creating new og image for ${hash} at: ${ogImageDir}`);
   const imagePath = `${ogImageDir}/${hash}.jpeg`;
   const publicPath = `/images/og/${hash}.jpeg`;
-
+  
+  console.log(`Creating new og image for ${hash} at: ${ogImageDir}`);
+  
   try {
     fs.statSync(imagePath);
     console.log(`Image already created for ${hash}. Returning.`);
-
     return publicPath;
   } catch (error) {
     // If this is hit, it means the file has not been created yet, so
     // the rest of the function will run
   }
 
+  const browser = await playwright.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath(),
+    headless: true,
+  });
+  
   console.log(`Taking screenshot for ${hash}.`);
   const page = await browser.newPage();
   await page.setViewportSize({
@@ -51,7 +56,7 @@ export async function createOgImage(path: string) {
 
   console.log(`Writing to file for ${hash}: `, imagePath);
   fs.mkdirSync(ogImageDir, { recursive: true });
-  fs.writeFileSync(imagePath, buffer);
+  fs.writeFileSync(imagePath, new Uint8Array(buffer));
 
   return publicPath;
 }
