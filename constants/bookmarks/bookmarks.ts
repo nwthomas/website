f2bd7bda59a717544d9851a9cd018b4ed88b9cd6
Nathan Thomas <nwthomas@me.com>
2025-12-01 09:51:49 -0800

    Add gpt oss tricks article to bookmarks

---
+++ constants/bookmarks/bookmarks.ts
@@ -0,0 +1,29 @@
+export type Bookmark = {
+  date: string;
+  url: string;
+};
+
+export type Bookmarks = Array<Bookmark>;
+
+export const BOOKMARKS: Bookmarks = [
+  {
+    date: "2025-12-01",
+    url: "https://huggingface.co/blog/faster-transformers",
+  },
+  {
+    date: "2025-12-01",
+    url: "https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-quantization",
+  },
+  {
+    date: "2025-11-10",
+    url: "https://berkshirehathaway.com/news/nov1025.pdf",
+  },
+  {
+    date: "2025-10-30",
+    url: "https://www.fast.ai/posts/2025-10-30-build-to-last.html",
+  },
+  {
+    date: "2025-07-29",
+    url: "https://www.wheresyoured.at/the-haters-gui",
+  },
+];
--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -1,33 +1,8 @@
+import { BOOKMARKS } from "../constants/bookmarks/bookmarks";
 import { CONTENTS_ID } from "../constants/routes";
 import { JOURNAL_PAGE_NAME } from "../constants/seo";
 import Layout from "../components/Layout";
 import styled from "@emotion/styled";
-
-type Bookmark = {
-  date: string;
-  url: string;
-};
-
-type Bookmarks = Array<Bookmark>;
-
-const BOOKMARKS: Bookmarks = [
-  {
-    date: "2025-12-01",
-    url: "https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-quantization",
-  },
-  {
-    date: "2025-11-10",
-    url: "https://berkshirehathaway.com/news/nov1025.pdf",
-  },
-  {
-    date: "2025-10-30",
-    url: "https://www.fast.ai/posts/2025-10-30-build-to-last.html",
-  },
-  {
-    date: "2025-07-29",
-    url: "https://www.wheresyoured.at/the-haters-gui",
-  },
-];
 
 function Bookmarks({ ogImage }) {
   return (
