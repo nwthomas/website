dd70a787d1e7251eeadec394ace49c6907525fed
Nathan Thomas <nwthomas@me.com>
2025-12-02 17:28:55 -0800

    Add article on death of software engineering

--- constants/bookmarks/bookmarks.ts
+++ constants/bookmarks/bookmarks.ts
@@ -7,6 +7,10 @@ export type Bookmarks = Array<Bookmark>;
 
 export const BOOKMARKS: Bookmarks = [
   {
+    date: "2025-12-02",
+    url: "https://www.jasonscheirer.com/weblog/vignettes",
+  },
+  {
     date: "2025-12-01",
     url: "https://vram.asmirnov.xyz"
   },
