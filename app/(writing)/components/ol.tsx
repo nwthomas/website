c5a54385f899ecf2703eb0977a49324434001dd8
Nathan Thomas <nwthomas@me.com>
2025-12-29 16:28:20 -0800

    Make writing pages headers render with ids

--- app/(writing)/components/ol.tsx
+++ app/(writing)/components/ol.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export function Ol({ children }: { children: ReactNode }) {
   return (
     <div className="w-full flex justify-center mt-5">
-      <ol className="w-full max-w-2xl list-inside">{children}</ol>
+      <ol className="w-full max-w-2xl list-inside mx-5">{children}</ol>
     </div>
   );
 }
--- app/(writing)/components/ul.tsx
+++ app/(writing)/components/ul.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export function Ul({ children }: { children: ReactNode }) {
   return (
     <div className="w-full flex justify-center mt-5">
-      <ul className="w-full max-w-2xl list-inside">{children}</ul>
+      <ul className="w-full max-w-2xl list-inside mx-5">{children}</ul>
     </div>
   );
 }
