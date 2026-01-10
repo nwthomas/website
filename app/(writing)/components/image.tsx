77feef19946f97f052ef47aaf1d0a466fb81b4c8
Nathan Thomas <nwthomas@me.com>
2026-01-09 16:54:57 -0800

    Allow wide prop for images in articles

--- app/(writing)/components/image.tsx
+++ app/(writing)/components/image.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import NextImage from "next/image";
+import { clsx } from "clsx";
 import { showImageOverlay } from "@/app/store/reducers/writingSlice";
 import { useDispatch } from "react-redux";
 
@@ -13,11 +14,14 @@ type Props = {
   src: string;
   title?: string;
   width: number;
+  wide?: boolean;
 };
 
-export function Image({ alt = "", height = 0, index, width = 0, src }: Props) {
+export function Image({ alt = "", height = 0, index, width = 0, src, wide }: Props) {
   const dispatch = useDispatch();
   const shouldPreload = index === 0;
+
+  console.log("wide", wide);
 
   const handleImageClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
@@ -36,10 +40,13 @@ export function Image({ alt = "", height = 0, index, width = 0, src }: Props) {
     <div className="w-full flex justify-center">
       <button
         aria-label="Enlarge image"
-        className="w-full max-w-2xl mb-5 hover:opacity-80 cursor-zoom-in transition-opacity duration-200 mx-5 items-center flex overflow-hidden"
+        className={clsx(
+          "w-full mb-5 hover:opacity-80 cursor-zoom-in transition-opacity duration-200 mx-5 items-center flex overflow-hidden",
+          wide ? "max-w-4xl" : "max-w-2xl",
+        )}
         onClick={handleImageClick}
       >
-        <div className={`aspect-ratio-[${width}/${height}] block leading-none relative w-full`}>
+        <div className={`aspect-ratio-[${width}/${height}] leading-none relative w-full flex justify-center`}>
           <NextImage
             alt={alt}
             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO5WZ5kAAAAASUVORK5CYII="
