d201d994d05b9531ef08f59dcb31f8eb69dbc89d
Nathan Thomas <nwthomas@me.com>
2026-03-17 21:27:24 -0700

    Update bookmarks with vite+

--- app/bookmarks/bookmarks.ts
+++ app/bookmarks/bookmarks.ts
@@ -13,6 +13,12 @@ export type Bookmarks = Array<Bookmark>;
 
 export const BOOKMARKS: Bookmarks = [
   {
+    id: "6163af08-6292-4141-b0f6-9ad665c60e28",
+    date: "2026-03-17",
+    title: "vite+"
+    url: "https://viteplus.dev",
+  },
+  {
     id: "6163af08-6292-4141-b0f6-9ad665c60e27",
     date: "2026-03-16",
     title: "Tanstack AI",
